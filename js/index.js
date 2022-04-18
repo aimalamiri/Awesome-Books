9f868b18c3860f6fc171bc945929ed4d36cff6fc
Bassem <bassem@MacBook-Pro-de-Bassem.local>
2022-04-18 17:32:54 -0300

    create addBook function to add books to the page

--- js/index.js
+++ js/index.js
@@ -1,17 +1,29 @@
-const bookForm = document.querySelector('#book-form');
-const bookTitle = document.querySelector('#book-title');
-const bookAuthor = document.querySelector('#book-author');
+const bookForm = document.querySelector("#book-form");
+const booksList = document.getElementById("book-list");
+const bookTitle = document.querySelector("#book-title");
+const bookAuthor = document.querySelector("#book-author");
 
-const booksList = [];
+const booksData = [];
 
-bookForm.addEventListener('submit', () => {
+bookForm.addEventListener("submit", () => {
   const book = {
     id: Math.round(Math.random() * 10000000),
     title: bookTitle.value,
     author: bookAuthor.value,
   };
+AddBook(book)
+  booksData.push(book);
+  bookTitle.value = "";
+  bookAuthor.value = "";
+});
 
-  booksList.push(book);
-  bookTitle.value = '';
-  bookAuthor.value = '';
-})
+function AddBook (book) {
+  const bookElement = `
+ 
+ <li>
+ <h2>${book.title}<h2/>
+ <span>${book.author}<span/>
+ <button data-id="book.id" class="remove">Remove<button/>
+ <li/> `;
+ booksList.innerHTML += bookElement;
+}
