10fcd7156c55003641169d7bb57572c36e142b67
Aimal Amiri <meaimal@hotmail.com>
2022-04-19 21:33:36 +0430

    Add Book and Library classes with adding functionality

--- js/index.js
+++ js/index.js
@@ -3,60 +3,64 @@ const booksList = document.getElementById('book-list');
 const bookTitle = document.querySelector('#book-title');
 const bookAuthor = document.querySelector('#book-author');
 
-const booksData = [];
-
-function AddBook(book) {
+function insertBookIntoDom(book) {
   const bookElement = `
-    <li class="book">
-      <h5>${book.title}</h5>
-      <h6>${book.author}</h6>
-      <button data-id="${book.id}" class="book-remove">Remove</button>
-    </li>`;
+      <li class="book">
+        <h5>${book.title}</h5>
+        <h6>${book.author}</h6>
+        <button data-id="${book.id}" class="book-remove">Remove</button>
+      </li>`;
   booksList.innerHTML += bookElement;
 }
 
-function storageWriter() {
-  localStorage.setItem('books', JSON.stringify(booksData));
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
+    this.id = Math.round(Math.random() * 10000000);
+  }
 }
 
-bookForm.addEventListener('submit', () => {
-  const book = {
-    id: Math.round(Math.random() * 10000000),
-    title: bookTitle.value,
-    author: bookAuthor.value,
-  };
-  AddBook(book);
-  booksData.push(book);
-  storageWriter();
+class Library {
+  constructor() {
+    this.books = [];
+  }
+
+  #saveIntoStorage() {
+    localStorage.setItem('books', JSON.stringify(this.books));
+  }
+
+  #getFromStorage() {
+    return JSON.parse(localStorage.getItem('books'));
+  }
+
+  addBook(book) {
+    this.books.push(book);
+    insertBookIntoDom(book);
+    this.#saveIntoStorage();
+  }
+
+  load() {
+    const booksData = this.#getFromStorage();
+    if (booksData) {
+      for (let i = 0; i < booksData.length; i += 1) {
+        this.books.push(booksData[i]);
+        insertBookIntoDom(booksData[i]);
+      }
+    }
+  }
+}
+
+const library = new Library();
+
+bookForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const book = new Book(bookTitle.value, bookAuthor.value);
+  library.addBook(book);
   bookTitle.value = '';
   bookAuthor.value = '';
 });
 
 window.addEventListener('load', () => {
-  const books = JSON.parse(localStorage.getItem('books'));
-  if (books) {
-    for (let i = 0; i < books.length; i += 1) {
-      booksData.push(books[i]);
-      AddBook(books[i]);
-    }
-  }
-});
-
-function removeBook(e) {
-  const id = e.target.getAttribute('data-id');
-  for (let i = 0; i < booksData.length; i += 1) {
-    if (booksData[i].id === Number(id)) {
-      booksData.splice(i, 1);
-      booksList.innerHTML = '';
-      for (let i = 0; i < booksData.length; i += 1) {
-        AddBook(booksData[i]);
-      }
-      storageWriter();
-      break;
-    }
-  }
-}
-
-document.addEventListener('click', (e) => {
-  removeBook(e);
+  library.load();
 });
